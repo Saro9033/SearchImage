5b41670596fb58d61d7444fa98ab1bf1e69972ac
Saro9033 <ssaravanan2310@gmail.com>
2023-07-23 18:35:26 +0530

    final

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
-
+  const sty = {padding:'0 15px 0 15px', textDecoration:'none', color:'white', fontSize:'17px'}
     return (
         <div className="navContainer">
             <h3>Search Images </h3>
             <div className="navList">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
+                <Link style={sty} to="/">Home</Link>
+                <Link style={sty} to="/about">About</Link>
+                <Link style={sty} to="/contact">Contact</Link>
             </div>
         </div>
     );
